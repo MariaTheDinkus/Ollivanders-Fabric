ab92300b0ec9ce18909bfd3a44d3401852d230bf
Jacob Farley <jacobfarley2017@gmail.com>
2019-09-22 23:04:21 -0500

    Setup proper Server -> Client syncing for spell projectiles.

--- src/main/java/com/zundrel/ollivanders/Ollivanders.java
+++ src/main/java/com/zundrel/ollivanders/Ollivanders.java
@@ -1,14 +1,20 @@
 package com.zundrel.ollivanders;
 
+import com.zundrel.ollivanders.common.entity.EntitySpellProjectile;
 import com.zundrel.ollivanders.common.events.CommonEventHandler;
 import com.zundrel.ollivanders.common.registry.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.UUID;
 
 public class Ollivanders implements ModInitializer
 {
@@ -16,6 +22,8 @@ public class Ollivanders implements ModInitializer
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     public static ItemGroup generalItemGroup = FabricItemGroupBuilder.build(new Identifier(MODID, "general"), () -> new ItemStack(ModItems.PHOENIX_FEATHER));
+
+    public static final Identifier SPELL_PROJECTILE_PACKET = new Identifier(MODID, "spell_projectile_packet");
 
     @Override
     public void onInitialize() {
@@ -31,6 +39,25 @@ public class Ollivanders implements ModInitializer
         ModSpells.init();
         ModCommands.init();
 
-        //ServerSidePacketRegistry.INSTANCE.register(new Identifier(MODID, "spell_projectile_packet"), );
+        ClientSidePacketRegistry.INSTANCE.register(SPELL_PROJECTILE_PACKET, ((packetContext, packetByteBuf) -> {
+            double x = packetByteBuf.readDouble();
+            double y = packetByteBuf.readDouble();
+            double z = packetByteBuf.readDouble();
+            float pitch = packetByteBuf.readFloat();
+            float yaw = packetByteBuf.readFloat();
+            int entityId = packetByteBuf.readInt();
+            UUID uuid = packetByteBuf.readUuid();
+
+            packetContext.getTaskQueue().execute(() -> {
+                Entity entity = new EntitySpellProjectile(packetContext.getPlayer().getEntityWorld(), x, y, z);
+
+                entity.updateTrackedPosition(x, y, z);
+                entity.pitch = (pitch * 360) / 256.0F;
+                entity.yaw = (yaw * 360) / 256.0F;
+                entity.setEntityId(entityId);
+                entity.setUuid(uuid);
+                ((ClientWorld) packetContext.getPlayer().getEntityWorld()).addEntity(entityId, entity);
+            });
+        }));
     }
 }
